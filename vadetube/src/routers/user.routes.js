a2e178535833f635924de1191a95d5cfaee68d42
arbaz khan <itsarbaz51@gmail.com>
2024-04-01 12:14:18 +0530

    add register controller and bug fixes

--- vadetube/src/routers/user.routes.js
+++ vadetube/src/routers/user.routes.js
@@ -1,8 +1,20 @@
 import { Router } from "express"
 import { registerUsers } from "../controllers/user.controller.js"
+import {upload} from "../middleware/multer.middleware.js"
 
 const router = Router()
 
-router.route("/register").post(registerUsers)
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUsers)
 
 export default router
--- vadetube/src/utils/ApiResponse.js
+++ vadetube/src/utils/ApiResponse.js
@@ -10,3 +10,5 @@ class ApiResponse {
         this.success = statusCode < 100
     }
 }
+
+export {ApiResponse}
