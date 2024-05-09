10d7f365ef8a9875235cbfe6d6facbaf547166a4
arbaz khan <itsarbaz51@gmail.com>
2024-05-09 10:56:17 +0530

    Fix logout functionality and remove console.log statement

--- vadetube/src/routers/user.routes.js
+++ vadetube/src/routers/user.routes.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import {
   changeCurrentPassword,
   getCurrentUser,
+  updateAccountDetails,
   getUserChannelProfile,
   getWatchHistory,
   loginUser,
   logoutUser,
+  refreshAccessToken,
   registerUsers,
   updateCoverImage,
   updateUserAvatar,
@@ -35,14 +37,14 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
-router.route("/current-user").get(verify.JWT, getCurrentUser);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 router
   .route("/avatar")
   .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router
-  .route("cover-image")
-  .patch(verifyJWT, upload.single("/coverImage"), updateCoverImage);
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 router.route("/history").get(verifyJWT, getWatchHistory);
 
